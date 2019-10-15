5f18ddcb304a2fbc40911508b569d89aebb82c0b
Carlos Camara <carlos.camara.ortiz@gmail.com>
2019-10-14 21:50:23 -0500

    add initial project files

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+// IMPORTACIONES
+import Sum from './models/Sum'
+
+import * as sumView from './views/SumView'
+
+import styles from '../css/style.css'
+import sassStyles from '../scss/style.scss'
+
+//Calculadora
+
+document.querySelector("#sumar").addEventListener(("click"), () => {
+    event.preventDefault()
+
+    sumView.clearArea()
+
+    const numA = sumView.parseInputs(sumView.getInputNumA)
+    const numB = sumView.parseInputs(sumView.getInputNumB)
+
+    const resultado = Sum(numA, numB)
+
+    return sumView.renderSumView(resultado)
+})
+
+//Fin calculadora
